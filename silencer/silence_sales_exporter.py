33ba08467830a702c7a0ea5db2025781a7874ae6
Karl <karl.rink@nationsinfocorp.com>
2021-03-05 16:31:02 -0800

    🍀 utc

--- silencer/silence_sales_exporter.py
+++ silencer/silence_sales_exporter.py
@@ -5,7 +5,7 @@ import requests
 url = "http://prom1:9093/alertmanager/api/v2/silences"
 
 start = "2021-03-09T03:00:00.000000" #7pm PST
-end   = "2021-03-10T15:00:00.000000" #7am PST
+end   = "2021-03-09T15:00:00.000000" #7am PST
 
 req = requests.post(url, json={
     "matchers": [
